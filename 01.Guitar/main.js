828c05d700456e007f7476f4edae5d19ee817aa7
jenmwa <jenmwa@gmail.com>
2023-02-18 19:30:20 +0100

    :tada: challenge day 1

---
+++ 01.Guitar/main.js
@@ -0,0 +1,24 @@
+function playSound(e) {
+  const key = e.key.toLowerCase();
+  const audio = document.querySelector(`audio[data-key='${key}']`);
+  const keyElement = document.querySelector(`.key[data-key='${key}']`);
+
+  if (!audio) {
+    return;
+  }
+
+  audio.currentTime = 0;
+  audio.play();
+  keyElement.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform")
+    return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
